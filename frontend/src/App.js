e2d9d4ca5a521b71cd4b7afa7de688fdd7717dfb
babakapa97 <mrseileen@mail.ru>
2023-05-01 13:35:36 +0800

    Рабочая версия бэк+фронт

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicketListComponent from './components/TicketListComponent'
+
+function App() {
+  return (
+    <div>
+      <TicketListComponent />
+    </div>
+  );
+}
+console.log();
+export default App;
