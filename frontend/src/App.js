0b734cab1dd83648f3f32a8790a95d0e8bd8c514
babakapa97 <mrseileen@mail.ru>
2023-05-06 19:05:17 +0800

    Зачатки интерфейса

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,37 @@
-import './App.css';
-import TicketListComponent from './components/TicketListComponent'
-import Auth from './components/Auth'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TicketList from './pages/TicketList'
+import HNaviBar from './components/HNaviBar';
+import VNaviBar from './components/VNaviBar';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import Auth from './components/Auth';
+import KnowBase from './pages/KnowBase'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
+import LoginForm from "./pages/LoginForm"
 
 function App() {
   return (
   <div>
-<TicketListComponent />
-</div>
+    
+    <Container fluid>
+    <Row><HNaviBar /></Row>
+    <Stack gap={3}>
+    <BrowserRouter>
+    <Row>
+     <Col sm={2}><VNaviBar /></Col>
+    <Routes>
+    <Route path="/" element={<Col><LoginForm /></Col>} /> 
+    <Route path="/ticket_list" element={<Col><TicketList /></Col>} />
+    <Route path="/knowbase" element={<KnowBase />} />
+    </Routes>
+    </Row>
+    </BrowserRouter>
+    </Stack>
+    </Container>
+  </div>
+
   );
 }
 console.log();
